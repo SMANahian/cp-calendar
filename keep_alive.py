785c5344102efcafaf226d47f7859efc62091fb5
S.M.A Nahian <smanahian123@gmail.com>
2022-02-15 09:58:11 +0000

    small bug fixed

--- keep_alive.py
+++ keep_alive.py
@@ -6,16 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    raw_bytes = ""
-    with open(CALENDAR_ICS_FILE_PATH, 'rb') as r:
-        for line in r:
-            raw_bytes = raw_bytes + line
-    response = make_response(raw_bytes)
+    response = make_response(open(CALENDAR_ICS_FILE_PATH).read())
     response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
     return response
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
 
 def keep_bot_alive():
     t = Thread(target=run)
