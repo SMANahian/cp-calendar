90b8365e41ffbcc3d81e63c4e5c896a160102c44
S.M.A Nahian <smanahian123@gmail.com>
2022-05-30 19:44:23 +0600

    Major bug fixed Hope it will work now :)

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from clist_api import load_contests, load_resources
 from website import create_app
 from time import sleep
+from threading import Thread
 
 load_dotenv()
 
 def main():
     app = create_app()
-    app.run(host='0.0.0.0',port=8080)
+    server = Thread(target=app.run)
+    server.start()
 
     while True:
         load_resources()
